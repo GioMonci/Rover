7fc51b5821c5d05cfbe9c0c0ab2e3fe1371845c8
GM <95672640+GioMonci@users.noreply.github.com>
2025-11-19 12:33:30 -0500

    Add files via upload

---
+++ Rover/Rover.ino
@@ -0,0 +1,18 @@
+/********************************
+*   Names: Giovanni M & Simon M
+*  Course: Embedded Systems
+*    Date: 11/19/25
+* Purpose: Move Rover
+*********************************/
+
+/******Pin Assignments*****/
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
